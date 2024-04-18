b94ecd171b3483208e6a65cb60e275e244e58628
matheus sants evangelista <matheus_santos_evangelista@hotmail.com>
2024-04-18 12:09:27 -0300

    add add logo to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,7 @@ function Footer() {
                         </a>
                     </div>
                     
-                {/* <img src="logo.png" alt="Logo" width={100} height={100} /> */}
+                <img src="logo.jpeg" alt="Logo" width={150} height={150} />
                 </ul>
             </footer>
         </>
