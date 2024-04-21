2e2dd70838334a31cebabff6c9a027b10a9122f5
matheus sants evangelista <matheus_santos_evangelista@hotmail.com>
2024-04-21 17:24:49 -0300

    mudando imagem

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { FaInstagram } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 import './Footer.css';
-import { color } from 'framer-motion';
 function Footer() {
     return (
         <>
@@ -11,21 +10,27 @@ function Footer() {
                 <ul className="contacts">
                     
                     <h1 style={{color:'black'}}>Formas de contato:</h1>
-                        <div className='wrapper'>
-                        <a className='wrapper' href="https://wa.me/554192181012">
+                        
+                        <a className='wrapper' style={{display:'flex',alignItems:'center'}} href="https://wa.me/554192181012">
                             <FaWhatsapp size={40} color='black' />
                             <p style={{color:"black"}}>Whatsapp</p>
                         </a>
-                        <a className='wrapper' href="https://www.instagram.com/reforma_truckevangelista/">
+                        <a className='wrapper' style={{display:'flex',alignItems:'center'}} href="https://www.instagram.com/reforma_truckevangelista/">
                             <FaInstagram size={40} color='black' />
                             <p style={{color:"black"}}>Instagram</p>
                         </a>
-                        <a className='wrapper' style={{ color:'black'}} href="mailto:claudio.evangelista@gmail.com">
+                        <a className='wrapper-email' style={{ color:'black',display:'flex',alignItems:'center'}} href="mailto:claudio.evangelista@gmail.com">
                             claudio.evangelista@gmail.com
                         </a>
-                    </div>
-                    
-                <img src="logo.png" alt="Logo" width={150} height={150} />
+                        <div className='wrapper-local' style={{ color:'black',display:'flex',alignItems:'center'}}>
+                            <p style={{color:'black'}}>
+                            Rua  das ametista n•378
+                            Jardim  Ouro fino
+                            </p>
+                        </div>
+                        <div className='wrapper' >
+                            <img src="logo.png" alt="Logo" width={150} height={150} />
+                        </div>
                 </ul>
             </footer>
         </>
